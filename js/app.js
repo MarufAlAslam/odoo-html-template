1ef0586799782f998952ba502d250062e09036d9
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-05-06 02:22:17 +0600

    slider done

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: false,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '#prevBtn',
+    nextArrow: '#nextBtn',
+})
