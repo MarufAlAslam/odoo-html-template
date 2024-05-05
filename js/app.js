f5f0cde01e0333e200a8d00b587016d44907909e
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-05-06 03:26:06 +0600

    reponsiveness done

--- js/app.js
+++ js/app.js
@@ -11,4 +11,27 @@ $('.slider').slick({
     arrows: true,
     prevArrow: '#prevBtn',
     nextArrow: '#nextBtn',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
 })
